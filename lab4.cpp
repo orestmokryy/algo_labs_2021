1f6fddbefc4a8229e38252efe430294133f7e030
BrigYeet <orest.mokryy@gmail.com>
2021-11-23 13:55:25 +0200

    Removed the graph i accidently committed

--- lab4.cpp
+++ lab4.cpp
@@ -74,39 +74,6 @@ int main()
 	graph->edge[0].dest = 1; // destination Vertices
 	graph->edge[0].weight = -1; // take a wild guess on what this is
 
-	
-	graph->edge[1].src = 0;
-	graph->edge[1].dest = 2;
-	graph->edge[1].weight = 4;
-
-	
-	graph->edge[2].src = 1;
-	graph->edge[2].dest = 2;
-	graph->edge[2].weight = 3;
-
-	
-	graph->edge[3].src = 1;
-	graph->edge[3].dest = 3;
-	graph->edge[3].weight = 2;
-
-	graph->edge[4].src = 1;
-	graph->edge[4].dest = 4;
-	graph->edge[4].weight = 2;
-
-	graph->edge[5].src = 3;
-	graph->edge[5].dest = 2;
-	graph->edge[5].weight = 5;
-
-
-	graph->edge[6].src = 3;
-	graph->edge[6].dest = 1;
-	graph->edge[6].weight = 1;
-
-	
-	graph->edge[7].src = 4;
-	graph->edge[7].dest = 3;
-	graph->edge[7].weight = -3;
-
 	BellmanFord(graph, 0); //where the int is the source vertice
 
 	return 0;
