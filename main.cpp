ac24c42cba69e6bcc641ab2933fdb4d5e5b3d16f
BrigYeet <orest.mokryy@gmail.com>
2022-01-04 17:54:46 +0200

    Added a solution for WChain problem

--- main.cpp
+++ main.cpp
@@ -1,63 +1,34 @@
 #include <bits/stdc++.h>
+#include <fstream>
+
 using namespace std;
+class Solution {
+   public:
+   static bool cmp(string s1, string s2){
+      return s1.size() < s2.size();
+   }
+   int longestStrChain(vector<string>& words) {
+      unordered_map <string, int> dp;
+      int n = words.size();
+      sort(words.begin(), words.end(), cmp);
+      int ret = 0;
+      for(int i = 0; i < n; i++){
+         int best = 0;
+         for(int j = 0; j < words[i].size(); j++){
+            string word = words[i].substr(0, j) +
+            words[i].substr(j + 1);
+            best = max(best, dp[word] + 1);
+         }
+         dp[words[i]] = best;
+         ret = max(ret, dp[words[i]]);
+      }
+      return ret;
+   }
+};
 
-#define alphabet 256 //possible alphabet characters
 
-void search(char pat[], char txt[], int q)
-{
-	int pattern_length = strlen(pat);
-	int text_lenght = strlen(txt);
-	int i, j;
-	int pattern_hash = 0;
-	int text_hash = 0; 
-	int h = 1;
-
-
-	for (i = 0; i < pattern_length - 1; i++)
-		h = (h * alphabet) % q;  
-
-	for (i = 0; i < pattern_length; i++)     //hash the pattern and the text
-	{
-		pattern_hash = (alphabet * pattern_hash + pat[i]) % q;
-		text_hash = (alphabet * text_hash + txt[i]) % q;
-	}
-
-	for (i = 0; i <= text_lenght - pattern_length; i++)  //the searching mechanism
-	{
-		if ( pattern_hash == text_hash )
-		{
-			bool flag = true;
-			for (j = 0; j < pattern_length; j++)
-			{
-				if (txt[i+j] != pat[j])
-				{
-				flag = false;
-				break;
-				}
-				
-			}
-
-			if (j == pattern_length)
-				cout<<"Pattern found at index "<< i<<endl;
-		}
-
-		if ( i < text_lenght-pattern_length )
-		{
-			text_hash = (alphabet*(text_hash - txt[i]*h) + txt[i+pattern_length])%q;
-
-			if (text_hash < 0)
-			text_hash = (text_hash + q);
-		}
-	}
-}
-
-int main()
-{
-	char txt[] = "is this a test and you should panik";
-	char pat[] = "is";
-		
-	int hashing_number = 101; //prime int for hashing
-                 //the bigger the number the less colisions you will get, but the computation will get more complex
-	search(pat, txt, hashing_number);
-	return 0;
+main(){
+   vector<string> v = {"a","b","ba","bca","bda","bdca"};
+   Solution ob;
+   cout << (ob.longestStrChain(v));
 }
